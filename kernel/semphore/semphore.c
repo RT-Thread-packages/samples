102d1fdb8b8abbb0760bc813800765f5819ee601
balanceTWK <linuxtwk@outlook.com>
2018-04-08 18:42:16 +0800

    [add semphore test]

--- kernel/semphore/semphore.c
+++ kernel/semphore/semphore.c
@@ -1,135 +1,132 @@
-/*
- * 程序清单：动态信号量
- *
- * 这个例子中将创建一个动态信号量（初始值为0 ）及一个动态线程，在这个动态线程中
- * 将试图采用超时方式去持有信号量，应该超时返回。然后这个线程释放一次信号量，并
- * 在后面继续采用永久等待方式去持有信号量， 成功获得信号量后返回。
- */
+#include <stdio.h>
 #include <rtthread.h>
-#include "tc_comm.h"
 
-/* 指向线程控制块的指针 */
-static rt_thread_t tid = RT_NULL;
+/* 信号量控制块 */
+static struct rt_semaphore static_sem;
 /* 指向信号量的指针 */
-static rt_sem_t sem = RT_NULL;
-/* 线程入口 */
-static void thread_entry(void* parameter)
+static rt_sem_t dynamic_sem = RT_NULL;
+
+ALIGN(RT_ALIGN_SIZE)
+static char thread1_stack[1024];
+struct rt_thread thread1;
+static void rt_thread_entry1(void* parameter)
 {
     rt_err_t result;
     rt_tick_t tick;
 
+/* 1. staic semaphore demo */
     /* 获得当前的OS Tick */
     tick = rt_tick_get();
 
-    /* 试图持有一个信号量，如果10个OS Tick依然没拿到，则超时返回 */
-    result = rt_sem_take(sem, 10);
+    /* 试图持有信号量，最大等待10个OS Tick后返回 */
+    result = rt_sem_take(&static_sem, 10);
     if (result == -RT_ETIMEOUT)
     {
-        rt_tick_t new_tick = rt_tick_get();
-        /* 可以有两个 tick 的误差 */
-        if (new_tick - tick >= 12)
+        /* 超时后判断是否刚好是10个OS Tick */
+        if (rt_tick_get() - tick != 10)
         {
-            rt_kprintf("tick error to large: expect: 10, get %d\n",
-                       new_tick - tick);
-
-            /* 如果失败，则测试失败 */
-            tc_done(TC_STAT_FAILED);
-            rt_sem_delete(sem);
+            rt_sem_detach(&static_sem);
             return;
         }
-        rt_kprintf("take semaphore timeout\n");
+        rt_kprintf("\ntake semaphore timeout\n");
     }
     else
     {
-        /* 因为并没释放信号量，应该是超时返回，否则测试失败 */
-        tc_done(TC_STAT_FAILED);
-        rt_sem_delete(sem);
+        /* 因为没有其他地方释放信号量，所以不应该成功持有信号量，否则测试失败 */
+        rt_kprintf("\ntake a static semaphore, failed.\n");
+        rt_sem_detach(&static_sem);
         return;
     }
 
     /* 释放一次信号量 */
-    rt_sem_release(sem);
+    rt_sem_release(&static_sem);
 
-    /* 继续持有信号量，并永远等待直到持有到信号量 */
-    result = rt_sem_take(sem, RT_WAITING_FOREVER);
+    /* 永久等待方式持有信号量 */
+    result = rt_sem_take(&static_sem, RT_WAITING_FOREVER);
     if (result != RT_EOK)
     {
-        /* 返回不正确，测试失败 */
-        tc_done(TC_STAT_FAILED);
-        rt_sem_delete(sem);
+        /* 不成功则测试失败 */
+        rt_kprintf("\ntake a static semaphore, failed.\n");
+        rt_sem_detach(&static_sem);
         return;
     }
 
-    /* 测试成功 */
-    tc_done(TC_STAT_PASSED);
-    /* 删除信号量 */
-    rt_sem_delete(sem);
-}
+    rt_kprintf("\ntake a staic semaphore, done.\n");
 
-int semaphore_dynamic_init()
-{
-    /* 创建一个信号量，初始值是0 */
-    sem = rt_sem_create("sem", 0, RT_IPC_FLAG_FIFO);
-    if (sem == RT_NULL)
+    /* 脱离信号量对象 */
+    rt_sem_detach(&static_sem);
+
+/* 2. dynamic semaphore test */
+
+    tick = rt_tick_get();
+
+    /* 试图持有信号量，最大等待10个OS Tick后返回 */
+    result = rt_sem_take(dynamic_sem, 10);
+    if (result == -RT_ETIMEOUT)
     {
-        tc_stat(TC_STAT_END | TC_STAT_FAILED);
-        return 0;
+        /* 超时后判断是否刚好是10个OS Tick */
+        if (rt_tick_get() - tick != 10)
+        {
+            rt_sem_delete(dynamic_sem);
+            return;
+        }
+        rt_kprintf("\ntake semaphore timeout\n");
     }
-
-    /* 创建线程 */
-    tid = rt_thread_create("thread",
-                           thread_entry, RT_NULL, /* 线程入口是thread_entry, 入口参数是RT_NULL */
-                           THREAD_STACK_SIZE, THREAD_PRIORITY, THREAD_TIMESLICE);
-    if (tid != RT_NULL)
-        rt_thread_startup(tid);
     else
-        tc_stat(TC_STAT_END | TC_STAT_FAILED);
+    {
+        /* 因为没有其他地方释放信号量，所以不应该成功持有信号量，否则测试失败 */
+        rt_kprintf("\ntake a dynamic semaphore, failed.\n");
+        rt_sem_delete(dynamic_sem);
+        return;
+    }
+
+    /* 释放一次信号量 */
+    rt_sem_release(dynamic_sem);
+
+    /* 永久等待方式持有信号量 */
+    result = rt_sem_take(dynamic_sem, RT_WAITING_FOREVER);
+    if (result != RT_EOK)
+    {
+        /* 不成功则测试失败 */
+        rt_kprintf("\ntake a dynamic semaphore, failed.\n");
+        rt_sem_delete(dynamic_sem);
+        return;
+    }
+
+    rt_kprintf("\ntake a dynamic semaphore, done.\n");
+    /* 删除信号量对象 */
+    rt_sem_delete(dynamic_sem);
+}
+/* 线程示例的初始化 */
+int thread_sample_init()
+{
+    rt_err_t result;
+
+    /* 初始化静态信号量，初始值是0 */
+    result = rt_sem_init(&static_sem, "ssem", 0, RT_IPC_FLAG_FIFO);
+    if (result != RT_EOK)
+    {
+        rt_kprintf("\ninit dynamic semaphore failed.\n");
+        return -1;
+    }
+
+    /* 创建一个动态信号量，初始值是0 */
+    dynamic_sem = rt_sem_create("dsem", 0, RT_IPC_FLAG_FIFO);
+    if (dynamic_sem == RT_NULL)
+    {
+        rt_kprintf("\ncreate dynamic semaphore failed.\n");
+        return -1;
+    }
+
+    rt_thread_init(&thread1,
+                   "thread1",
+                   rt_thread_entry1,
+                   RT_NULL,
+                   &thread1_stack[0],
+                   sizeof(thread1_stack),11,5);
+    rt_thread_startup(&thread1);
 
     return 0;
 }
+INIT_APP_EXPORT(thread_sample_init);
 
-#ifdef RT_USING_TC
-static void _tc_cleanup()
-{
-    /* 调度器上锁，上锁后，将不再切换到其他线程，仅响应中断 */
-    rt_enter_critical();
-
-    if (sem)
-    {
-        rt_sem_delete(sem);
-        sem = RT_NULL;
-    }
-
-    /* 删除线程 */
-    if (tid != RT_NULL && tid->stat != RT_THREAD_CLOSE)
-    {
-        rt_thread_delete(tid);
-    }
-
-    /* 调度器解锁 */
-    rt_exit_critical();
-
-    /* 设置TestCase状态 */
-    tc_done(TC_STAT_PASSED);
-}
-
-int _tc_semaphore_dynamic()
-{
-    /* 设置TestCase清理回调函数 */
-    tc_cleanup(_tc_cleanup);
-    semaphore_dynamic_init();
-
-    /* 返回TestCase运行的最长时间 */
-    return 100;
-}
-/* 输出函数命令到finsh shell中 */
-FINSH_FUNCTION_EXPORT(_tc_semaphore_dynamic, a dynamic semaphore example);
-#else
-/* 用户应用入口 */
-int rt_application_init()
-{
-    semaphore_dynamic_init();
-
-    return 0;
-}
-#endif
