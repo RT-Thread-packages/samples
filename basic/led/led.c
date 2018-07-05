625d00cbd4b3b26f99684d436be108270d4617be
yaomo718 <yaomo718@163.com>
2018-07-05 10:04:34 +0800

    代码简化
    对led控制部分代码做了简化

--- basic/led/led.c
+++ basic/led/led.c
@@ -16,81 +16,7 @@ static struct rt_thread led_thread;
 
 void rt_hw_led_init(void)
 {
-    rt_pin_mode(LED_0_PIN, PIN_MODE_INPUT);
-    rt_pin_mode(LED_1_PIN, PIN_MODE_INPUT);
-}
-
-void rt_hw_led_on(rt_uint32_t n)
-{
-    switch (n)
-    {
-    case 0:
-        rt_pin_write(LED_0_PIN, 1);
-        break;
-    case 1:
-        rt_pin_write(LED_0_PIN, 1);
-        break;
-    default:
-        break;
-    }
-}
-
-void rt_hw_led_off(rt_uint32_t n)
-{
-    switch (n)
-    {
-    case 0:
-        rt_pin_write(LED_0_PIN, 0);
-        break;
-    case 1:
-        rt_pin_write(LED_0_PIN, 0);
-        break;
-    default:
-        break;
-    }
-}
-
-static rt_uint8_t led_inited = 0;
-void led(rt_uint32_t led, rt_uint32_t value)
-{
-    /* init led configuration if it's not inited. */
-    if (!led_inited)
-    {
-        rt_hw_led_init();
-        led_inited = 1;
-    }
-
-    if (led == 0)
-    {
-        /* set led status */
-        switch (value)
-        {
-        case 0:
-            rt_hw_led_off(0);
-            break;
-        case 1:
-            rt_hw_led_on(0);
-            break;
-        default:
-            break;
-        }
-    }
-
-    if (led == 1)
-    {
-        /* set led status */
-        switch (value)
-        {
-        case 0:
-            rt_hw_led_off(1);
-            break;
-        case 1:
-            rt_hw_led_on(1);
-            break;
-        default:
-            break;
-        }
-    }
+    rt_pin_mode(LED_PIN, PIN_MODE_OUTPUT);
 }
 
 static void led_thread_entry(void *parameter)
@@ -104,13 +30,13 @@ static void led_thread_entry(void *parameter)
         /* led1 on */
         rt_kprintf("led on, count : %d\r\n", count);
         count++;
-        rt_hw_led_on(0);
+        rt_pin_write(LED_PIN, 0);
         rt_thread_delay(RT_TICK_PER_SECOND / 2); /* sleep 0.5 second and switch to other thread */
 
         /* led1 off */
         rt_kprintf("led off\r\n");
 
-        rt_hw_led_off(0);
+        rt_pin_write(LED_PIN, 1);
         rt_thread_delay(RT_TICK_PER_SECOND / 2);
     }
 }
@@ -134,9 +60,9 @@ int led_sample_init(void)
     }
     return 0;
 }
-#ifdef RT_SAMPLES_AUTORUN
+	/* 如果设置了RT_SAMPLES_AUTORUN，则加入到初始化线程中自动运行 */
+#if defined (RT_SAMPLES_AUTORUN) && defined(RT_USING_COMPONENTS_INIT)
     INIT_APP_EXPORT(led_sample_init);
 #endif
 /* 导出到 msh 命令列表中 */
 MSH_CMD_EXPORT(led_sample_init, led sample);
-
